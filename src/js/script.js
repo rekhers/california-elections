bb6fd3455b8e49faae6460b9cc776dfcd6c7c8d4
Rekha Tenjarla <rtenjarla@theatlantic.com>
2019-09-14 20:07:56 -0400

    add svg loader and import cali svg

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,10 @@ import "babel-polyfill";
 import * as d3 from "d3";
 
 
-function scaryClown() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve('🤡');
-      }, 2000);
-    });
-  }
-  
-  async function msg() {
-    const msg = await scaryClown();
-    console.log('Message:', msg);
-  }
-  
-  msg();
+d3.select('.acali')
+.attr("width", "100%")
+.attr("height", "auto")
+.style("stroke", "black")
+.style("stroke-width", "1.5")
+.style("fill", "white")
+
