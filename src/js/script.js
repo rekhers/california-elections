0ada1442143132ec4f1838ee1a78f1b291b18484
Rekha Tenjarla <rtenjarla@theatlantic.com>
2019-09-15 23:03:09 -0400

    add card data

--- src/js/script.js
+++ src/js/script.js
@@ -2,23 +2,29 @@ import "babel-polyfill";
 import * as d3 from "d3";
 
 /*
-* Initialize the map
+*
+* Initialize the map by awaiting our three data files
+*
 */
 async function init () {
-   const svg = await insertSVG();
+   await insertSVG();
    const data = await d3.csv('/data/ca-votes.csv');
    const fips = await d3.csv('/data/ca-fips.csv');
 
    handleData(data, fips);
 }
 
+
+/*
+*
+* Rolls up data, adds fips code, rolls it up once more such that its keyed by fips
+*
+*/
 function handleData(data, fips) {
-   //rollup data by county
    const counties = d3.nest()
    .key((d) => { return d.county; })
    .object(data);
-
-
+   
    const matched = fips.map((f) => {
       const name = f.county.trim();
       return {'name': name, 'votes': counties[name], 'fips': f.fips};
@@ -27,7 +33,6 @@ function handleData(data, fips) {
    const newMatched = d3.nest()
       .key((d) => { return d.fips})
       .object(matched)
-
 
    buildMap(newMatched);
 }
@@ -48,6 +53,21 @@ function insertSVG () {
     })
 }
 
+function buildCards(county) {
+   console.log(county)
+   const cardContainer = document.querySelector(".card-container");
+   const card = document.createElement('div');
+   cardContainer.appendChild(card);
+   card.innerHTML = `<h3 class="cardTitle"> ${county[0].name} </h3> 
+   <ul class="list"> 
+    <li> ${county[0].votes[0].candidate} : ${county[0].votes[0].votes}  </li>
+    <li> ${county[0].votes[1].candidate} : ${county[0].votes[1].votes}  </li>
+    <li> ${county[0].votes[2].candidate} : ${county[0].votes[2].votes}  </li>
+    <li> ${county[0].votes[3].candidate} : ${county[0].votes[3].votes}  </li>
+   </ul>`
+
+   }
+
 function getColor(county) {
    const results = county[0].votes;
    const lookup = {};
@@ -59,11 +79,18 @@ function getColor(county) {
          lookup[r.candidate] = r.votes;
       })
    }
- 
+   
+   /*
+   * 
+   * Return red if Trump won, blue if Clinton won, and orange if neither won
+   * 
+   */
    if(parseInt(lookup[republican]) > parseInt(lookup[democrat])) {
-      return "red"
+      return "rgb(184, 24, 0)"
+   } else if(parseInt(lookup[democrat]) > parseInt(lookup[republican])) {
+      return "rgb(0, 125, 214)"
    } else {
-      return "blue"
+      return "orange"
    }
 }
 
@@ -78,6 +105,7 @@ function buildMap(data) {
   [...paths].forEach((path) => {
       const color = getColor(data[path.id]);
       document.getElementById(path.id).setAttribute('fill', color);
+      buildCards(data[path.id]);
   })
 
 }
